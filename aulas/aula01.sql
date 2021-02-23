3eaab1448066651e02e64877b65104a9208cad3d
Lucas Mesquita de Souza <79462136+lucasmsouza24@users.noreply.github.com>
2021-02-23 13:36:59 -0300

    Update aula01.sql

--- aulas/aula01.sql
+++ aulas/aula01.sql
@@ -27,11 +27,81 @@ insert into Aluno values (02211016, 'Fernando Martins','Taboão');
 select * from Aluno; 
 
 -- Inserção de dados de mais alunos
-insert into Aluno values (02211041, 'Leo Nunes', 'Ferraz de Vasconcelos'),
-                         (02211042, 'Lucas Henrique Adelino', 'Ponte Grande'),
-                         (02211044, 'Lucas Mesquita', 'Jardim São Luis'),
-                         (02211057, 'Roberta Pires', 'Oswaldo Cruz'),
-                         (02211022, 'Gabriel Sanchez', 'Scarpelli');
+insert into Aluno values
+(02211041, 'Leo Nunes', 'Ferraz de Vasconcelos'),
+(02211042, 'Lucas Henrique Adelino', 'Ponte Grande'),
+(02211044, 'Lucas Mesquita', 'Jardim São Luis'),
+(02211057, 'Roberta Pires', 'Oswaldo Cruz'),
+(02211022, 'Gabriel Sanchez', 'Scarpelli');
                          
 -- Exibir os dados dos alunos que são do bairro 'Taboão'
 select * from Aluno where bairro = 'Taboão';
+
+-- insert rafa
+insert into Aluno values(02211054, 'Rafael Gomes', 'Jardim São José');
+
+-- exibir estrutura da tabela
+desc Aluno;
+
+-- exibir os alunos cujo RA estejam entre um valor e outro
+select * from aluno where ra between 02211040 and 02211050;
+
+-- exibir os alunos cujo RA seja diferente de um determinado valor 
+select * from aluno where ra > 02211045;
+-- ou
+select * from aluno where ra != 02211022;
+
+-- exibir dados da tabela ordenados pelo nome do aluno
+select * from aluno order by nome asc;
+
+-- exibir dados da tabela ordenados de forma descrescente pelo nome do aluno
+select * from aluno order by nome desc;
+
+-- exibir dados da tabela ordenados pelo bairro
+select * from aluno order by bairro;
+
+-- exibir dados dos alunos cujo RA termine com 4
+select * from aluno where ra like '%4';
+
+-- exibir dados de alunos cujo bairro comece com a letra J
+select * from aluno where bairro like 'J%';
+
+-- exibir dados de alunos cujo nome termine com s 
+select * from aluno where nome like '%s';
+
+-- exibir dados de alunos cujo a segunda letra é "e" 
+select * from aluno where nome like '_e%';
+
+-- exibir dados de alunos cujo a segunda letra do bairro seja a letra "a"
+select * from aluno where bairro like '_a%';
+
+-- exibir os dados de alunos cujo a penúltima letra do nome seja "e"
+select * from aluno where nome like '%e_';
+
+-- exibir os dados de alunos cujo a penúltimas letras do primeiro nome e sobrenome sejam "e"
+select * from aluno where nome like '%e_ %';
+
+-- exibir os dados dos alunos cujo sobrenome termine com a letra 's' ou com a letra 'z'
+select * from aluno where nome like '%s' or nome like '%z';
+
+select * from aluno where nome like '% %';
+
+-- exibir os dados dos alunos cujo RA seja diferente 2211022 ou 2211054 ou 2211042
+select * from aluno where ra not in (2211022, 2211054, 2211042);
+
+-- atualizando valores
+-- alterando bairro do aluno de ra 2211016 para 'Vila Santa Luzia'
+update aluno set bairro = 'Vila Santa Luzia' where ra = 2211016;
+
+-- alterar o bairro e o nome do aluno de ra = 2211054
+update aluno set nome = 'Rafael Faria Gomes', bairro = 'Olímpico' 
+where ra = 2211054;
+
+-- excluir o aluno de ra = 2211044
+delete from aluno where ra = 2211044;
+
+-- excluir tabela
+drop table aluno;
+
+-- excluir banco de dados
+drop database aluno;
